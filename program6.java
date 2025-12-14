7ecb169bc12ad387a5e1abc0e7765c1894d6feb9
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-14 15:07:51 +0530

    add program6

---
+++ program6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class FtoC {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Fahrenheit: ");
+            double f = sc.nextDouble();
+            double c = (f - 32) * 5 / 9;
+            System.out.println("Celsius = " + c);
+        }
+    }
+}
